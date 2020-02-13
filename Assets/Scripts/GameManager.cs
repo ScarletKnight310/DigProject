634ea66c226a9631a2b86ba6d83dbd71729dfb6f
Kashod Cagnolatti <kcagnolatti@callutheran.edu>
2020-02-13 13:46:41 -0800

    Lvl Progression Fix
    Deletes previous lvl

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,11 @@ public class GameManager : MonoBehaviour
 
     public GameObject gm;
     public GameObject player;
+    public int depthInc = 50;
+
     GroundManager manager;
+    int level = 1;
+
 
     // Start is called before the first frame update
     void Awake()
@@ -20,9 +24,20 @@ public class GameManager : MonoBehaviour
     {
         if (player.transform.position.y <= (-1*manager.MaxDepth)) {
             player.transform.position = new Vector3(0, 0, 0);
-            manager.MaxDepth = manager.MaxDepth*2;
+            if (level <= 5)
+            {
+                manager.MaxDepth = manager.MaxDepth * 2;
+
+            }
+            else
+            {
+                manager.MaxDepth = manager.MaxDepth + depthInc;
+
+            }
+            manager.removeLevel();
             manager.resetBottom();
             manager.PlaceBlocks();
+            level++;
         }
     }
 }
