63ac1884305ca1ceb4238353a3783c09663eea28
Kashod Cagnolatti <kcagnolatti@callutheran.edu>
2020-02-20 12:57:59 -0800

    Controller Support

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
    
-    [Header("Controls")]
+    [Header("Controls For")]
     public KeyCode left = KeyCode.A;
     public KeyCode right = KeyCode.D;
     public KeyCode up = KeyCode.W;
     public KeyCode down = KeyCode.X;
+    public bool isController = false;
+
     private Rigidbody player_body = null;
     [Space]
     public float speed = 3f;
     public bool AutoMove = true;
-    private int directx = 0;
-    private int directy = 0;
-    
+    private float directx = 0;
+    private float directy = 0;
+    private float tHold = 0.2f;
 
     void Start()
     {
@@ -25,9 +27,30 @@ public class Movement : MonoBehaviour
         player_body.useGravity = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (isController) {
+            GamePadControls();
+        } else {
+            KeyBoardControls();
+        }
+    }
+
+    private void GamePadControls() {
+        float xB = Input.GetAxis("Horizontal");
+        float yB = Input.GetAxis("Vertical");
+
+        //float x = xB > tHold || xB < tHold ? Mathf.Sign(xB) : 0;
+        //float y = yB > tHold || yB < tHold ? Mathf.Sign(yB) : 0;
+
+        if (xB != 0)
+            directx = xB;
+        if (yB != 0)
+            directy = yB;
+        //Debug.Log(directx + ", " + directy);
+    }
+
+    private void KeyBoardControls() {
         if (Input.GetKey(right)) {
             directx = 1;
             directy = 0;
@@ -44,7 +67,7 @@ public class Movement : MonoBehaviour
             directy = -1;
             directx = 0;
         }
-        // dia
+        //
         if (Input.GetKey(right) && Input.GetKey(up)) {
             directy = 1;
             directx = 1;
@@ -61,16 +84,11 @@ public class Movement : MonoBehaviour
             directy = -1;
             directx = -1;
         }
-        //if (Input.GetKey(right) && Input.GetKey(left))
-        // directx = 0;
-        //transform.position = new Vector3(transform.position.x + (direct * speed * Time.deltaTime), transform.position.y, transform.position.z);
-        //Debug.Log("Move "+ direct);
-        //player.AddForce(transform.right*(direct * speed),ForceMode.Force);
-       // if(!AutoMove)
-        //    Reset();
     }
     private void FixedUpdate() {
-        player_body.MovePosition(new Vector3((directx * speed * Time.deltaTime) + transform.position.x, (directy * speed * Time.deltaTime) + transform.position.y, 0));
+        player_body.MovePosition(new Vector3((directx * speed * Time.deltaTime) + transform.position.x, 
+            (directy * speed * Time.deltaTime) + transform.position.y, 
+            0));
     }
 
     private void Reset()
