7f8f7955b7dbf0bf8d288fb25c3f72645ef17c93
Kashod Cagnolatti <kcagnolatti@callutheran.edu>
2020-02-27 10:24:05 -0800

    The Controls are now intergrated into the Unity Input System
    also blocks disable instead of delete

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
    
-    [Header("Controls For")]
-    public KeyCode left = KeyCode.A;
-    public KeyCode right = KeyCode.D;
-    public KeyCode up = KeyCode.W;
-    public KeyCode down = KeyCode.X;
-    public bool isController = false;
+    
 
     private Rigidbody player_body = null;
-    [Space]
+    [Header("Move Control")]
     public float speed = 3f;
     public bool AutoMove = true;
     private float directx = 0;
@@ -29,14 +24,6 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        if (isController) {
-            GamePadControls();
-        } else {
-            KeyBoardControls();
-        }
-    }
-
-    private void GamePadControls() {
         float xB = Input.GetAxis("Horizontal");
         float yB = Input.GetAxis("Vertical");
 
@@ -50,41 +37,6 @@ public class Movement : MonoBehaviour
         //Debug.Log(directx + ", " + directy);
     }
 
-    private void KeyBoardControls() {
-        if (Input.GetKey(right)) {
-            directx = 1;
-            directy = 0;
-        }
-        if (Input.GetKey(left)) {
-            directx = -1;
-            directy = 0;
-        }
-        if (Input.GetKey(up)) {
-            directy = 1;
-            directx = 0;
-        }
-        if (Input.GetKey(down)) {
-            directy = -1;
-            directx = 0;
-        }
-        //
-        if (Input.GetKey(right) && Input.GetKey(up)) {
-            directy = 1;
-            directx = 1;
-        }
-        if (Input.GetKey(right) && Input.GetKey(down)) {
-            directy = -1;
-            directx = 1;
-        }
-        if (Input.GetKey(left) && Input.GetKey(up)) {
-            directy = 1;
-            directx = -1;
-        }
-        if (Input.GetKey(left) && Input.GetKey(down)) {
-            directy = -1;
-            directx = -1;
-        }
-    }
     private void FixedUpdate() {
         player_body.MovePosition(new Vector3((directx * speed * Time.deltaTime) + transform.position.x, 
             (directy * speed * Time.deltaTime) + transform.position.y, 
