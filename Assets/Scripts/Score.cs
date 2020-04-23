06116a54dac9290b90583523abcc90afda162232
Kashod Cagnolatti <45503314+ScarletKnight310@users.noreply.github.com>
2020-04-23 16:31:18 -0700

    Revert "Merging AI / premaster pt.1"
    This reverts commit 3af608dbb1f6b270319cb54f636cb391d31e3d33.

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,34 +27,3 @@ public class Score : MonoBehaviour
         PlayingPanel.Instance.ShowScore(score);
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Score : MonoBehaviour
-{
-    public static int score;
-
-    void Start()
-    {
-        score = 0;
-    }
-
-    // add/remove points to score
-    public static void updateScore(int points)
-    {
-        score = score + points;
-        PlayingPanel.Instance.ShowScore(score);
-
-        Debug.Log("Score: " + score);
-    }
-
-    // set score back to 0
-    public static void resetScore()
-    {
-        score = 0;
-        PlayingPanel.Instance.ShowScore(score);
-    }
-}
->>>>>>> EnemyAi
