fda838d85ece7378326da79ec93e76a259ca31c0
Kashod Cagnolatti <45503314+ScarletKnight310@users.noreply.github.com>
2020-04-27 19:45:55 -0700

    barebones Main Menu added
    - Start game button ->
    Starts Prototype scene
    - credits button->
    Toggles credits
    - quit game button ->
    closes game

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string lvlName;
+    public GameObject picture;
+    public GameObject credits;
+
+    public void NewGame() {
+        SceneManager.LoadScene(lvlName);
+    }
+
+    public void Credits() {
+        picture.SetActive(!picture.activeSelf);
+        credits.SetActive(!credits.activeSelf);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
